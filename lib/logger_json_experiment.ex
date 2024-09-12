6546dc2914b8b4ec1cc567577a5044fe66ae1d20
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-12 19:34:24 +0900

    mix new logger_json_experiment

---
+++ lib/logger_json_experiment.ex
@@ -0,0 +1,18 @@
+defmodule LoggerJsonExperiment do
+  @moduledoc """
+  Documentation for `LoggerJsonExperiment`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoggerJsonExperiment.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/logger_json_experiment_test.exs
@@ -0,0 +1,8 @@
+defmodule LoggerJsonExperimentTest do
+  use ExUnit.Case
+  doctest LoggerJsonExperiment
+
+  test "greets the world" do
+    assert LoggerJsonExperiment.hello() == :world
+  end
+end
